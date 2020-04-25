31e83898b4e9d6a76cf4521fe0a6a9ee32ea58c6
Arthur Lawson <48330779+artlawson@users.noreply.github.com>
2020-04-25 02:24:59 +0000

    Add files via upload

--- baseline.cc
+++ baseline.cc
@@ -5,10 +5,12 @@
 #include <iostream>
 #include <vector>
 #include <thread>
+#include <mutex>
 
-const int N_REQ;
+const int N_REQ = 318;
+const int N_THREADS = 3;
 
-std::vector<double> baseline_latencies(int N_REQ){
+std::vector<double> baseline_latencies(int nreq){
 
     thread_local Cache* cache = new Cache("127.0.0.1","6969");
 
@@ -53,33 +55,36 @@ std::pair<double,double> baseline_performance(int num){
 }
 
 // An example from lecture notes
-void threaded_performance(const int nthreads)
+std::pair<double, double> threaded_performance(const int nthreads)
+//changed return type to pair from void ~AJizzle with the blizzle
 {
 	
-	std::vector<std::thread> thread_reqper_results;
-	threads.reserve(nthreads); // for efficiency, allocate space for nthreads
-	std::vector<std::thread> thread_percentile_results;
+	std::vector<std::thread> thread_reqper_results; //not quite sure what you meant to do with this so I just put all the "threads" vector references as this
+	thread_reqper_results.reserve(nthreads); // for efficiency, allocate space for nthreads
+	std::vector<double> thread_percentile_results;
+    std::vector<double> thread_nreq_results; //IArthur created to match what you 
 
 
-	auto latencies_mutex = std::mutex(); // set up mutexes? muteces? mutecies? muteces?
+	auto latencies_mutex = std::mutex(); // set up mutexes? muteces? mutecies? muteces? MUTECI?!
 	auto performance_mutex = std::mutex();
+ 
 
 	auto run_one_thread = [&]() { //each thread will run baseline_latencies and return the result
 		
-		auto guard = std::scoped_lock(best_mutex); // we're accessing protected data so lock!
+		auto guard = std::scoped_lock(performance_mutex); // we're accessing protected data so lock!
 		
-		const auto out_pair = baseline_performance(N_REQ);
+		const std::pair<double,double> out_pair = baseline_performance(N_REQ);
 
 		thread_nreq_results.push_back(out_pair.second);
-		thread_percentile_results.push_back(out_pair.first);
+		thread_percentile_results.push_back(out_pair.first); //i think u meant to declare this as a vector of doubles instead of threads...i"ll change it 
 
+	};
+
+	for (int i = 0; i < nthreads; ++i) { //add threads to vector
+		thread_reqper_results.push_back(std::thread(run_one_thread));
 	}
 
-	for (unsigned i = 0; i < nthreads; ++i) { //add threads to vector
-		threads.push_back(std::thread(run_one_thread));
-	}
-
-	for (auto& t : threads) { // join threads so they all can finish their work!
+	for (auto& t : thread_reqper_results) { // join threads so they all can finish their work!
 		t.join();
 	}
 
@@ -109,7 +114,7 @@ void threaded_performance(const int nthreads)
 
 int main() {
 
-	auto output = threaded_performance(nthreads);
+	auto output = threaded_performance(N_THREADS);
 	std::cout<<output.first<<"<- 95th percentile"<< output.second<<"<-req/per"<<std::endl;
 	
 	return 1;
